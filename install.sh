c365adceeed8e6786e8d0b85d4966e8a56098025
Zarcolio <registered@quicknet.nl>
2021-11-18 09:36:53 +0100

    Added argument -auto for auto installing 2ulb

--- install.sh
+++ install.sh
@@ -4,18 +4,28 @@ scriptname="wwwordlist.py"
 
 pip3 install -r requirements.txt
 
-dir=$(pwd)
+ddir=$(pwd)
 
 2ulb 2&>/dev/null
 	if [ $? -eq 127 ]
-	then	
-		while true; do
-			read -p "2ulb not found, install 2ulb? [y/n]: " yn
-			case $yn in
-			    [Yy]*) cd .. ; git clone https://github.com/Zarcolio/2ulb ; sudo python3 2ulb/2ulb.py 2ulb/2ulb.py ; cd $dir ; sudo 2ulb $scriptname; exit 0 ;;  
-			    [Nn]*) echo "Aborted" ; exit 1 ;;
-			esac
-		done
+	then
+		if [ "$1" = "-auto" ]
+		then
+			cd .. 
+			git clone https://github.com/Zarcolio/2ulb
+			sudo python3 2ulb/2ulb.py 2ulb/2ulb.py
+			cd $dir
+			sudo 2ulb $scriptname
+			exit 0
+		else
+			while true; do
+				read -p "2ulb not found, install 2ulb? [y/n]: " yn
+				case $yn in
+				    [Yy]*) cd .. ; git clone https://github.com/Zarcolio/2ulb ; sudo python3 2ulb/2ulb.py 2ulb/2ulb.py ; cd $dir ; sudo 2ulb $scriptname; exit 0 ;;
+				    [Nn]*) echo "Aborted" ; exit 1 ;;
+				esac
+			done
+		fi
 	else
 		sudo 2ulb $scriptname
 	fi
